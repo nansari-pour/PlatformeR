ec711c6d7a4245f0b10f6bd4313403ceb5da74a8
Naser Ansari-Pour <42205757+nansari-pour@users.noreply.github.com>
2024-03-04 15:00:54 +0000

    Update prop_mix.R

--- R/prop_mix.R
+++ R/prop_mix.R
@@ -1,6 +1,11 @@
-# Non-piecewise functions for calculating the proportion of downsampling from LOH/GAIN and normal BAMs
-
-# Correct function to calculate the prop for the LOH bam for subclonal events
+#' Non-piecewise function for calculating the proportion of downsampling from LOH and normal BAMs
+#' 
+#' Correct function to calculate the prop for the LOH bam for subclonal events
+#' @param cna_ccf The tumour purity to be simulated (numeric, ranging (0,1])
+#' @param chrom_length Chromosome sequence length (integer value)
+#' @param cna_length CNA sequence length (integer value)
+#' @author naser.ansari-pour
+#' @keywords internal
 
 loh_mix_prop <- function(cna_ccf,cna_length,chrom_length){
   loh_frac=(cna_ccf)*((2*chrom_length-cna_length)/(2*chrom_length))
@@ -8,7 +13,14 @@ loh_mix_prop <- function(cna_ccf,cna_length,chrom_length){
   return(round(loh_frac/(loh_frac+non_loh_frac),digits = 3))
 }
 
-# Correct function to calculate the prop for the GAIN bam for subclonal events
+#' Non-piecewise function for calculating the proportion of downsampling from GAIN and normal BAMs
+#' 
+#' Correct function to calculate the prop for the GAIN bam for subclonal events
+#' @param cna_ccf The tumour purity to be simulated (numeric, ranging (0,1])
+#' @param chrom_length Chromosome sequence length (integer value)
+#' @param cna_length CNA sequence length (integer value)
+#' @author naser.ansari-pour
+#' @keywords internal
 
 gain_mix_prop <- function(cna_ccf,cna_length,chrom_length){
   gain_frac=(cna_ccf)*((2*chrom_length+cna_length)/(2*chrom_length))
