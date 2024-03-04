b5b30e7c2b9c5051681caba56d76286b84a2a2be
Naser Ansari-Pour <42205757+nansari-pour@users.noreply.github.com>
2024-03-04 15:00:25 +0000

    Update genomewide_merge_bam_list.R

--- R/genomewide_merge_bam_list.R
+++ R/genomewide_merge_bam_list.R
@@ -1,4 +1,11 @@
-# genome-wide BAM merging input list generator
+#' Function to generate a list of bams to be merged for a genomewide level bam
+#' 
+#' @param chrom_names Name of the chromosomes to be used for merging their respective chromosome-level bams 
+#' @param simulated_purity The tumour purity to be simulated (numeric, ranging (0,1])
+#' @param out_dir Full path to the out directory where genomewide files will be written
+#' @param samplename Sample name to be used for the final genomewide bam file with simulated CNA
+#' @author naser.ansari-pour
+#' @export
 
 genomewide_merge_bam_list <- function(chrom_names,simulated_purity,out_dir,samplename){
   chrom_names=gsub("chr","",chrom_names)
