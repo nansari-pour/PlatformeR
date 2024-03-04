3e41e1e1e9ee905e1e889ef63b6a453784cc297e
Naser Ansari-Pour <42205757+nansari-pour@users.noreply.github.com>
2024-03-04 14:50:47 +0000

    Create runPlatformeR.R

---
+++ inst/scripts/runPlatformeR.R
@@ -0,0 +1,42 @@
+# PlatformeR wrapper run
+
+Args=commandArgs(trailingOnly = TRUE)
+cna_simulate_file = toString(Args[1])
+chrom = as.integer(toString(Args[2]))
+print(paste("PlatformeR wrapper run for Chromosome",chrom))
+out_dir = toString(Args[3])
+fasta_dir = toString(Args[4])
+phase_dir = toString(Args[5])
+ART = toString(Args[6])
+BWA = toString(Args[7])
+SAM = toString(Args[8])
+loh_haplotype = toString(Args[9])
+gain_haplotype = toString(Args[10])
+coverage = as.numeric(toString(Args[11]))
+normal_coverage = as.numeric(toString(Args[12]))
+ncores = as.integer(toString(Args[13]))
+simulated_purity = as.numeric(toString(Args[14]))
+
+print(paste("Coverage =",coverage,"Simulated Purity =",simulated_purity))
+
+PlatformeR(cna_simulate_file = cna_simulate_file,
+           chrom = chrom,
+           out_dir = out_dir,
+           fasta_dir = fasta_dir,
+           phase_dir = phase_dir,
+           art_bin = ART,
+           bwa = BWA,
+           samtools = SAM,
+           tmp_dir = "TMP",
+           ncores = ncores,
+           coverage = coverage/2,
+           read_length = 150,
+           fragment_size = 400,
+           fragment_size_sd = 10,
+           simulated_purity = simulated_purity,
+           loh_haplotype = loh_haplotype,
+           gain_haplotype = gain_haplotype,
+           simulated_purity = simulated_purity,
+           generate_normal = TRUE,
+           normal_coverage = normal_coverage/2,
+           generate_diploid_normal = TRUE)
