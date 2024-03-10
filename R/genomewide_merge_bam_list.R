5bfcea9f752cd4920b2784cd9fb73759cadb59d6
Naser Ansari-Pour <42205757+nansari-pour@users.noreply.github.com>
2024-03-10 23:24:18 +0000

    Update genomewide_merge_bam_list.R
    Allow for normal germline bam list to be generated in the same function

--- R/genomewide_merge_bam_list.R
+++ R/genomewide_merge_bam_list.R
@@ -4,18 +4,27 @@
 #' @param simulated_purity The tumour purity to be simulated (numeric, ranging (0,1])
 #' @param out_dir Full path to the out directory where genomewide files will be written
 #' @param samplename Sample name to be used for the final genomewide bam file with simulated CNA
+#' @param is.normal set to TRUE if the bam list is to be generated for normal germline chromosome-level bams
 #' @author naser.ansari-pour
 #' @export
 
-genomewide_merge_bam_list <- function(chrom_names,simulated_purity,out_dir,samplename){
+genomewide_merge_bam_list <- function(chrom_names,simulated_purity,out_dir,samplename,is.normal){
   chrom_names=gsub("chr","",chrom_names)
+  if (is.normal){
+    for (chrom in chrom_names){
+      bam_name=paste0(out_dir,"chr",chrom,"/chr",chrom,".normal.bam")
+      # write bam_name to a single file - input to merge_mini_bams function in out_dir
+      cat(bam_name, sep = "\n", file = paste0(out_dir,samplename,"_genomewide_bam_list.txt"), append = TRUE)
+    }
+  } else {
   for (chrom in chrom_names){
-      if (simulated_purity<1){
-        bam_name=paste0(out_dir,"chr",chrom,"/chr",chrom,".clone_purity_",simulated_purity,".bam")
-      } else {
+    if (simulated_purity<1){
+      bam_name=paste0(out_dir,"chr",chrom,"/chr",chrom,".clone_purity_",simulated_purity,".bam")
+    } else {
       bam_name=paste0(out_dir,"chr",chrom,"/chr",chrom,".clone.bam")
-      }
+    }
     # write bam_name to a single file - input to merge_mini_bams function in out_dir
     cat(bam_name, sep = "\n", file = paste0(out_dir,samplename,"_genomewide_bam_list.txt"), append = TRUE)
-    }
   }
+  }
+}
