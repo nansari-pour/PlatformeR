8aa0c09dfa5c8959a22f1c82bc4a98f9636764e3
Naser Ansari-Pour <42205757+nansari-pour@users.noreply.github.com>
2024-03-10 23:38:24 +0000

    Update WGS_bam_generator.R
    update genomewide_merge_bam_list arguments

--- inst/scripts/WGS_bam_generator.R
+++ inst/scripts/WGS_bam_generator.R
@@ -12,7 +12,7 @@ simulated_purity = as.numeric(toString(Args[2]))
 print(paste("Simulated purity =",simulated_purity))
 out_dir = toString(Args[3])
 print(out_dir)
-samplename = toString(Args[4])
+tumourname = toString(Args[4])
 samtools = toString(Args[5])
 ncores = as.integer(toString(Args[6]))
 # Optional - paired analysis
@@ -27,9 +27,10 @@ setwd(out_dir)
 print(getwd())
 
 genomewide_merge_bam_list(chrom_names = chrom_names,
-                      simulated_purity = simulated_purity,
-                      out_dir = out_dir,
-                      samplename = samplename)
+                          simulated_purity = simulated_purity,
+                          out_dir = out_dir,
+                          samplename = tumourname,
+                          is.normal = FALSE)
 
 merge_mini_bams(samtools = samtools,
                 mini_bams_list_file = paste0(out_dir,samplename,"_genomewide_bam_list.txt"),
@@ -37,11 +38,12 @@ merge_mini_bams(samtools = samtools,
                 clone_bam_filename = ifelse(simulated_purity==1,paste0(samplename,".bam"),paste0(samplename,"_purity_",simulated_purity,".bam")))
 
 if (merge_normal){
-
+  
   genomewide_merge_bam_list(chrom_names = chrom_names,
-                        simulated_purity = simulated_purity,
-                        out_dir = out_dir,
-                        samplename = normalname)
+                            simulated_purity = NULL,
+                            out_dir = out_dir,
+                            samplename = normalname,
+                            is.normal = TRUE)
 
   merge_mini_bams(samtools = samtools,
                   mini_bams_list_file = paste0(out_dir,normalname,"_genomewide_bam_list.txt"),
